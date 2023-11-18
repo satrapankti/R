b3cfc793108151a18dc51ed0733d183461753619
Pankti Satra <satrapankti26@gmail.com>
2023-11-18 16:48:39 +0530

    Add files via upload

---
+++ Deployment.R
@@ -0,0 +1,33 @@
+library(shiny)
+library(dplyr)
+install.packages("readr", dependencies = TRUE, repos="http://cran.rstudio.com/")
+ui <- fluidPage(
+  titlePanel("Prediction"),
+  sidebarLayout(
+    sidebarPanel(
+      numericInput("num","Hp",1),
+      numericInput("num1","VOL",1),
+      numericInput("num2","SP",1),
+      numericInput("num3","Weight",1)
+    ),
+    mainPanel(
+      tableOutput("distplot")
+      
+    )
+  )
+)
+server <- function(input, output) {
+  output$distplot <- renderTable({
+    
+    Cars <- read.csv("E:/ExcelR/Data Science(Andheri-Sadanand-Chirag)(21st Aug 2021)/Data Set Files/8.Cars.csv")
+    
+    model.car <- lm(MPG~.,data = Cars)
+    nw = data.frame(HP = input$num,VOL = input$num1,SP = input$num2,WT = input$num3)
+    nw
+    w = predict(model.car,nw)
+    w
+  })
+  
+}
+
+shinyApp(ui = ui, server = server)
